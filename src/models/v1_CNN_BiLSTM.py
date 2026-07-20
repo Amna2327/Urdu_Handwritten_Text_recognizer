4bd663743e6c6c1275903b00e910122d4b61148a
Amna Ahmed <ahmadalisukhera7849@gmail.com>
2026-07-20 20:27:16 +0500

    Fixed synatx error

--- src/models/v1_CNN_BiLSTM.py
+++ src/models/v1_CNN_BiLSTM.py
@@ -65,7 +65,7 @@ class v1_Recognizer(nn.Module):
         super().__init__()
         self.CNN_Encoder=CNN_Encoder()
         self.BiLSTM=nn.LSTM(input_size=256,hidden_size=256,batch_first=True,bidirectional=True,num_layers=1)
-        self.Dropout=nn.Dropout(rate=0.3)
+        self.Dropout=nn.Dropout(0.3)
         self.output_layer=nn.Linear(512,118)
     
 
